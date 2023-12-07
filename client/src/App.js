45c55e04c83bc1d8b61853202a3430ad18eafe07
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-12-07 13:18:22 -0500

    Created Courses Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
 
 import Header from './component/Header'
+import Courses from './component/Courses';
+import { Routes, Route } from 'react-router-dom';
 import "./styles/reset.css"
 import './styles/global.css';
+import ErrorDisplay from './component/ErrorDisplay';
+
+
+
 
 
 
 function App() {
+  
+  
   return (
     <div >
       <Header/>
-      {/* <Routes>
+      <Courses/>
+      <Routes>
+        <Route path="/error" element={<ErrorDisplay />} />
         <Route path="/" element={<Courses />} />
-        <Route path="/courses/create" element={<CreateCourses/>} />
+        {/* <Route path="/courses/create" element={<CreateCourses/>} />
         <Route path="/courses/:id" element={<CoursesDetail/>}/>
         <Route path="/courses/:id/update" element={<UpdateCourse />}/>
 
         <Route path="/signin" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
         <Route path="/signout" element={<UserSignOut  />} />
-        <Route path="*" element={<PhotoNotFound />} />
-      </Routes> */}
+        <Route path="*" element={<PhotoNotFound />} /> */}
+      </Routes>
 
 
       
---
+++ client/src/component/ErrorDisplay.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ErrorDisplay = ({errors}) => {
+    let errorsDisplay = null
+
+    if (errors.length){
+        errorsDisplay = (
+            <>
+            <div className="validation--errors">
+                    <h3>Validation Errors</h3>
+                    <ul>
+                  {errors.map((error, i) => <li key={i}>{error}</li>)}
+                </ul>
+                </div>
+            </>
+          ) 
+    }
+   return errorsDisplay
+}
+
+export default ErrorDisplay
